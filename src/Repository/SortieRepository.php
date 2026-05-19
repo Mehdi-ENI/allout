72f58c0525c194765e87f69dde2480fe53194869
Mehdi Rochereau <271727509+mehdi-rochereau@users.noreply.github.com>
2026-05-19 16:46:28 +0200

    ajout des filtres à la liste de sorties dynamique

--- src/Repository/SortieRepository.php
+++ src/Repository/SortieRepository.php
@@ -5,22 +5,26 @@ namespace App\Repository;
 use App\Entity\Sortie;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\SecurityBundle\Security;
 
 /**
  * @extends ServiceEntityRepository<Sortie>
  */
 class SortieRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
+    public function __construct(ManagerRegistry           $registry,
+                                private readonly Security $security)
     {
         parent::__construct($registry, Sortie::class);
     }
 
     public function findWithFilters(array $filters): array
     {
+        $user = $this->security->getUser();
+
         $qb = $this->createQueryBuilder('s');
 
-        if(!empty($filters['site'])) {
+        if (!empty($filters['site'])) {
             $qb->andWhere('s.site = :site')
                 ->setParameter('site', $filters['site']);
         }
@@ -31,9 +35,52 @@ class SortieRepository extends ServiceEntityRepository
         }
 
         if (!empty($filters['date_debut']) && empty($filters['date_fin'])) {
-            $qb->andWhere('s.dateHeureDebut >= :date_heure_debut')
+            $qb->andWhere('s.dateHeureDebut >= :date')
                 ->setParameter('date', $filters['date_debut']);
         }
+
+        if (empty($filters['date_debut']) && !empty($filters['date_fin'])) {
+            $qb->andWhere('s.dateHeureDebut <= :date')
+                ->setParameter('date', $filters['date_fin']);
+        }
+
+        if (!empty($filters['date_debut']) && !empty($filters['date_fin'])) {
+            $qb->andWhere('s.dateHeureDebut BETWEEN :date_debut AND :date_fin')
+                ->setParameter('date_debut', $filters['date_debut'])
+                ->setParameter('date_fin', $filters['date_fin']);
+        }
+
+        if (!empty($filters['sorties_organisees']) && $user) {
+            $qb->andWhere('s.organisateur = :organisateur')
+                ->setParameter('organisateur', $user);
+        }
+
+        if (!empty($filters['sorties_inscrits']) && $user) {
+            $qb->join('s.participants', 'p')
+                ->andWhere('p = :participant')
+                ->setParameter('participant', $user);
+        }
+
+        if (!empty($filters['sorties_non_inscrits']) && $user) {
+            $qb->andWhere(
+                $qb->expr()->not(
+                    $qb->expr()->isMemberOf(':participant', 's.participants')
+                )
+            )->setParameter('participant', $user);
+        }
+
+        if (!empty($filters['sorties_passees'])) {
+            $qb->andWhere('s.dateHeureDebut < :date')
+                ->setParameter('date', new \DateTime('NOW'));
+        }
+
+        $qb->orderBy('s.dateHeureDebut', 'ASC');
+
+        return $qb
+            ->getQuery()
+            ->getResult();
+
+
     }
 
 //    /**
