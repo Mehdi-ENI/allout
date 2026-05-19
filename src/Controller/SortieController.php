0be4dff57c8d04b593a850d290ecc47b4a49c566
ddiana26 <diana.dumitrescu2026@campus-eni.fr>
2026-05-19 14:29:16 +0200

    Ajout create et list basiques

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,19 +2,60 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use App\Form\SortieType;
 use App\Repository\SortieRepository;
+use App\Service\SortieService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/sortie', name: 'sortie_')]
 final class SortieController extends AbstractController
 {
-    #[Route('/sortie', name: 'app_sortie')]
-    public function create(SortieRepository $sortieRepository): Response
+    #[Route('/create', name: 'create')]
+    public function create(Request $request, SortieService $sortieService): Response
     {
 
+        $sortie = new Sortie();
+        $sortieForm = $this->createForm(SortieType::class, $sortie);
+        $sortieForm->handleRequest($request);
+        if ($sortieForm->isSubmitted() && $sortieForm->isValid()) {
+//            // Organisateur connecté
+//            $sortie->setOrganisateur($this->getUser());
 
-        return $this->render('sortie/create.html.twig');
+
+            try {
+
+                $sortieService->creerSortie($sortie);
+
+                $this->addFlash('success', 'Sortie créée avec succès');
+
+                return $this->redirectToRoute('sortie_list');
+
+            } catch (\Exception $e) {
+
+                dd($e->getMessage());
+                $this->addFlash('error', $e->getMessage());
+            }
+
+        }
+
+
+        return $this->render('sortie/create.html.twig', [
+            'sortieForm' => $sortieForm->createView(),
+        ]);
     }
+
+    #[Route('/list', name: 'list')]
+    public function list(SortieRepository $sortieRepository): Response
+    {
+        $sortie = new Sortie();
+        $sortie= $sortieRepository->findAll();
+        return $this->render('sortie/list.html.twig', [
+            'sorties' => $sortie,
+        ]);
+    }
+
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,10 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Lieu;
+use App\Entity\Participant;
+use App\Entity\Site;
+use App\Entity\Ville;
 use App\Enum\Etat;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -12,7 +16,47 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+
+        $site = new Site();
+        $site->setNom('Test');
+
+        $manager->persist($site);
+        $manager->flush();
+
+        $ville = new Ville();
+        $ville->setNom('Test');
+        $ville->setCodePostal('12345');
+        $manager->persist($ville);
+        $manager->flush();
+
+        $lieu = new Lieu();
+        $lieu->setNom('Test');
+        $lieu->setRue('Test');
+        $lieu->setVille($ville);
+        $manager->persist($lieu);
+        $manager->flush();
+
+
+
+        $organisateur = new Participant();
+        $organisateur->setNom('Test');
+        $organisateur->setPrenom('User');
+        $organisateur->setEmail('orga@test.com');
+        $organisateur->setPassword(password_hash('test', PASSWORD_BCRYPT));
+        $organisateur->setActif(true);
+        $organisateur->setTelephone('0123456789');
+
+        $organisateur->setRoles(['ROLE_USER']);
+        $organisateur->setActif(true);
+        $organisateur->setSite($site);
+
+        $manager->persist($organisateur);
+
+
+
         $faker = Factory::create('fr_FR');
+
+
 
         for ($i = 0; $i < 20; $i++) {
 
@@ -23,7 +67,12 @@ class AppFixtures extends Fixture
                 ->setDateLimiteInscription($faker->dateTimeBetween('-1 month', '+1 month'))
                 ->setNbInscriptionsMax($faker->numberBetween(10, 100))
                 ->setInfosSortie($faker->paragraph())
-                ->setEtat($faker->randomElement(Etat::cases()));
+                ->setEtat($faker->randomElement(Etat::cases()))
+                ->setOrganisateur($organisateur);
+            $sortie->setSite($site);
+            dump($sortie->getSite());
+            $sortie->setLieu($lieu);
+
             $manager->persist($sortie);
         }
         $manager->flush();
