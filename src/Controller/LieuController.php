067fa4735959ff5bd44918e1240a648d2b068b96
ddiana26 <diana.dumitrescu2026@campus-eni.fr>
2026-05-21 13:42:57 +0200

    ajout lieu dans formulaire creation sortie

--- src/Controller/LieuController.php
+++ src/Controller/LieuController.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/lieu')]
@@ -42,9 +43,15 @@ final class LieuController extends AbstractController
             $entityManager->persist($lieu);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_lieu_index', [], Response::HTTP_SEE_OTHER);
-        }
+//            return $this->redirectToRoute('app_lieu_index', [], Response::HTTP_SEE_OTHER);
 
+            if ($request->isXmlHttpRequest()) {
+                return new JsonResponse(['id' => $lieu->getId(), 'nom' => $lieu->getNom(),]);
+            }
+
+            return $this->redirectToRoute('app_lieu_index', [], Response::HTTP_SEE_OTHER);
+
+        }
         return $this->render('lieu/new.html.twig', [
             'lieu' => $lieu,
             'form' => $form,
--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\DTO\AnnulationDTO;
+use App\Entity\Lieu;
 use App\Entity\Sortie;
 use App\Form\AnnulationDTOType;
+use App\Form\LieuType;
 use App\Form\SortieType;
 use App\Repository\SiteRepository;
 use App\Repository\SortieRepository;
@@ -24,6 +26,10 @@ final class SortieController extends AbstractController
 
         $sortie = new Sortie();
         $sortieForm = $this->createForm(SortieType::class, $sortie);
+
+        $lieu = new Lieu();
+        $lieuForm = $this->createForm(LieuType::class, $lieu);
+
         $sortieForm->handleRequest($request);
         if ($sortieForm->isSubmitted() && $sortieForm->isValid()) {
             // Organisateur connecté
@@ -47,6 +53,7 @@ final class SortieController extends AbstractController
 
         return $this->render('sortie/create.html.twig', [
             'sortieForm' => $sortieForm->createView(),
+            'lieuForm' => $lieuForm->createView(),
         ]);
     }
 
